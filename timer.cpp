54fe0ba11ab3c08a2b50d8674b4b58f1f03631f9
sspeckens <sspeckens@gmail.com>
2015-08-01 21:54:54 +0200

    ver 1.01

--- timer.cpp
+++ timer.cpp
@@ -1,28 +1,16 @@
 #include "timer.h"
-#include <cstdlib>
-#include <string>
-#include <iostream>
-
-using namespace std;
+#include <windows.h>
 
 int Timer::SetTimer(int nbr_sec){
-	// shutdown -t xxsecond -s 	=>shutdown
-	string s1("shutdown -t ");
-	string s2;
-	s2=std::to_string(nbr_sec);
-	string s3(" -s >log_exe.txt 2>&1");
-	string s ;
-	s= s1+s2+s3;
-	system (s.c_str());
-	//system ("pause");
+	bool b;
+	InitiateSystemShutdown(NULL,NULL,nbr_sec,TRUE,FALSE);
+
 	return 0;
 }
 
 int Timer::CancelTimer(){
-	// shutdown -a 				=> abort previous shutdown command
-	string s("shutdown -a >log_exe.txt 2>&1");
-	system (s.c_str());
-	//system ("pause");	
+	bool b;
+	b=AbortSystemShutdown(NULL);
 	return 0;
 }
 
